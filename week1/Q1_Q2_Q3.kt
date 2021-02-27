deb007bafc55558152b7428b3a3706e5dedb033c
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2021-02-28 00:57:42 +0800

    Create Q1_Q2_Q3.kt

---
+++ week1/Q1_Q2_Q3.kt
@@ -0,0 +1,21 @@
+// Q1:
+
+    val id = 9527                  // (a) Correct.
+    // var number : Int = 5.6      // (b) INCORRECT. "The floating-point literal does not conform to the expected type Int"
+    var title = "Hello Kotlin!"    // (c) Correct.
+    // val content : String = null // (d) INCORRECT. "Null can not be a value of a non-null type String."
+                                   // The statement would be correct if it is changed to:
+                                   // val content : String? = null
+    val number : Double? = 556.6   // (e) Correct.
+    // var list : List<String>? = listOf(null, null)     // (f) INCORRECT. "Type mismatch: inferred type is String? but String was expected."
+                                                         // The statement would be correct if it is changed to:
+                                                         // var list : List<String?>? = listOf(null, null)
+    var list : List<String?>? = listOf("Wayne", "Chen")  // (g) Correct.
+    // list.size                   // (h) INCORRECT. "Only safe (?.) or non-null asserted (!!.) calls are allowed on a nullable receiver of type List<String?>?"
+                                   // The statement would be correct if it is changed to:
+                                   // list?.size   OR   list!!.size
+    list?.size                     // (i) Correct.
+
+
+
+// Q2:
