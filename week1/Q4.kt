21bd61c8cd051c4bd521d769c4d9113e3768dc14
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2021-02-28 18:33:51 +0800

    Create Q4.kt

---
+++ week1/Q4.kt
@@ -0,0 +1,16 @@
+// Q4's Answer:
+
+fun main() {
+
+    var aHuman : Human = Human("Jason Derulo")
+    aHuman.attack()
+
+}
+
+class Human(var name : String){ // class Human has a property called name
+    
+    fun attack() {
+        println("$name uses Fist Attack!")
+    }
+
+}
