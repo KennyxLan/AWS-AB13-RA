aa11fdd1dd703160a3cc7ceeae6d0c3e1f0093ff
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2021-02-28 18:40:41 +0800

    Update Q4.kt

--- week1/Q4.kt
+++ week1/Q4.kt
@@ -2,12 +2,12 @@
 
 fun main() {
 
-    var aHuman : Human = Human("Jason Derulo")
+    var aHuman: Human = Human("Jason Derulo")
     aHuman.attack()
 
 }
 
-class Human(var name : String){ // class Human has a property called name
+class Human(var name: String){ // class Human has a property called name
     
     fun attack() {
         println("$name uses Fist Attack!")
