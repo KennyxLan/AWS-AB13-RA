da20c24c8ee31dcad77f3b9c117b5205020cb897
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2021-03-07 16:26:35 +0800

    Update Q1_to_Q6.kt

--- week2/Q1_to_Q6.kt
+++ week2/Q1_to_Q6.kt
@@ -56,8 +56,32 @@ fun main(){
 
 
 //// Q3. What kind of key-value pair collection do we use in Android? Attach a sample code that uses a for-loop to print each key-value pair.
+We can use TreeMap to store key-value pairs.
 
+// sample code:
+import java.util.TreeMap
 
+fun main(){
+    
+    var cityPopulation = TreeMap<String, Long>()
+    cityPopulation["Taipei"] = 200
+    cityPopulation["Taoyuan"] = 150
+    cityPopulation["Taichung"] = 130
+    cityPopulation["KaoHsiung"] = 180
+    
+    for((city, population) in cityPopulation){
+        println("($city, $population)")
+    }
+    
+} 
+/* output:
+(KaoHsiung, 180)
+(Taichung, 130)
+(Taipei, 200)
+(Taoyuan, 150)
+*/
+
+// end of sample code
 
 
 
@@ -66,6 +90,7 @@ ArrayList 與 LinkedList 皆屬於線性資料結構。
 ArrayList 類似Vector，是透過array實作；當array空間已滿且需要擴大時，需先建立一個新的array，再將資料從舊array搬移到新array，較花時間。 --> 時間複雜度 O(n)
 LinkedList 是透過doubly linked list實作；在已知欲新增或刪除的node位置(pointer or reference)時，可立即新增或刪除node，不需一一搬移資料。 --> 時間複雜度 O(1)
 (ArrayList、LinkedList 在不同的operation下可能有不同的時間複雜度，須視情況採用合適的資料結構。)
+
 (Reference:  https://medium.com/zero-equals-false/arraylist-vs-linkedlist-vs-vector-22e1721a66b0  )
 
 
