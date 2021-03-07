634ddc18d65b866279edf78649672ab519df7ed5
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2021-03-07 15:39:37 +0800

    Update Q1_to_Q6.kt

--- week2/Q1_to_Q6.kt
+++ week2/Q1_to_Q6.kt
@@ -57,16 +57,50 @@ fun main(){
 //// Q4. What's the difference between ArrayList and LinkedList? Try to explain in Mandarin.
 ArrayList 與 LinkedList 皆屬於線性資料結構。
 ArrayList 類似Vector，是透過array實作；當array空間已滿且需要擴大時，需先建立一個新的array，再將資料從舊array搬移到新array，較花時間。 --> 時間複雜度 O(n)
-LinkedList 是透過doubly linked list實作；在已知欲新增或刪除的node位置(pointer)時，可立即新增或刪除node，不需一一搬移資料。 --> 時間複雜度 O(1)
+LinkedList 是透過doubly linked list實作；在已知欲新增或刪除的node位置(pointer or reference)時，可立即新增或刪除node，不需一一搬移資料。 --> 時間複雜度 O(1)
 (ArrayList、LinkedList 在不同的operation下可能有不同的時間複雜度，須視情況採用合適的資料結構。)
 (Reference:  https://medium.com/zero-equals-false/arraylist-vs-linkedlist-vs-vector-22e1721a66b0  )
 
 
-//// Q5.
+//// Q5. What are Value Type variables? List out the Value Type variables yout know.
+In contrast to Reference Type variables which hold references (to objects), Value Type variables are ones that hold values.
+Value Type varibels: Byte, Short, Int, Long, Float, Double
+
+// sample code A: ( Value Type variables' values cannot directly be swapped by a function. )
+fun swapByValue(a: Int, b: Int){
+    val temp: Int = a
+    a = b  // Error: Val cannot be reassigned
+    b = temp
+    println("In swapByValue(), after swap: a = $a, b = $b")
+} // end of sample code A
+
+
+// sample code B: ( Value Type variables' values, WHEN WRAPPED IN OBJECTS, can be swapped by a function. )
+class Wrapper(var integer: Int){
+}
+
+fun swapByReference(a: Wrapper, b: Wrapper){
+    val temp: Int = a.integer
+    a.integer = b.integer
+    b.integer = temp
+}
+
+fun main(){
+    var a: Wrapper = Wrapper(1)
+    var b: Wrapper = Wrapper(2)
+    println("Before swap:  a.integer = ${a.integer}, b.integer = ${b.integer}")   
+          // Before swap:  a.integer = 1, b.integer = 2
+    swapByReference(a, b)
+    println("After swap:   a.integer = ${a.integer}, b.integer = ${b.integer}")
+          // After swap:   a.integer = 2, b.integer = 1
+
+} // end of sample code B
+
+(Reference:  https://stackoverflow.com/questions/44515031/is-kotlin-pass-by-value-or-pass-by-reference  )
  
  
- 
-//// Q6.
+//// Q6. What's the maximum value of Int? If you want to represent integers larger than the limit, what should you do instead of using Int variables?
 Int變數(32bit)的數值範圍為	[ -2 147 483 648 , 2 147 483 647 ] 即 [ -2^31, 2^31 - 1 ] 。
 若要儲存超過此範圍的變數，可以使用Long (64bit)，其範圍為[ -9 223 372 036 854 775 808 , 9 223 372 036 854 775 807 ] 即 [ -2^63, 2^63 - 1]。
+ 
 (Reference:  https://kotlinlang.org/docs/basic-types.html  )
