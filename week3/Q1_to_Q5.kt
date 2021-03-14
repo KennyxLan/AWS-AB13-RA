206363684baaf9e2a7e4ca4b2ff3715602a8abbd
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2021-03-15 00:20:03 +0800

    Update Q1_to_Q5.kt

--- week3/Q1_to_Q5.kt
+++ week3/Q1_to_Q5.kt
@@ -17,4 +17,32 @@ The benefits of encapsulation includes:
 
 
 //// 3. How to use enum in Kotlin? Attach a sample code that uses enum in Kotlin.
+enum class Planet{
+    EARTH,
+    MARS,
+    JUPITER,
+    SATURN
+}
 
+fun main(){
+    println(Planet.values().size) // output:4
+    for(i in 0 until Planet.values().size){ // output:(0, EARTH)  (1, MARS)  (2, JUPITER)  (3, SATURN)
+        print("($i, ${Planet.values()[i]})  ")
+    }
+}
+
+
+
+
+
+//// 4. What are the differences between LinearLayout and RelativeLayout? Try to explain indetails.
+LinearLayout: The elements in a LinearLayout are placed one after another in a sequence. The `android:orientation` of a LinearLayout can be `vertical` or `horizontal`.
+RelativeLayout: The elements in a RelativeLayout are placed according to the relations between the elements.
+RelativeLayout may be more complicated to set up, when compared with LinearLayout, because we need to carefully specify the relations between the elements to achieve the desired layout.
+
+
+
+
+
+//// 5. Try to explain the benefits of ConstraintLayout. Why you should use it?
+ConstraintLayout is a versatile layout. We can use ConstraintLayout in a variety of situations, especially when we need to set up a complicated layout. We use `Design` in Android Studio to configure the ConstraintLayout.
