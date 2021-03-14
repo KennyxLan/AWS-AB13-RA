eb6d213af80d9c68ea2c0a7cba167d5de6c1e43d
KennyxLan <64935509+KennyxLan@users.noreply.github.com>
2021-03-14 21:51:58 +0800

    Create Q1_to_Q5.kt

---
+++ week3/Q1_to_Q5.kt
@@ -0,0 +1,12 @@
+//// 1. What is Encapsulation? Try to list out the benefits of Encapsulation.
+Encapsulation is an object-oriented programming practice where we wrap the data members (properties) and member functions (methods) inside a class.
+The benefits of encapsulation includes:
+- information hiding => We can hide the internal values / types of data members and implementation of a class.
+- access control => We can specify how an object can be accessed by its clients. For example, only get() is allowed but set() is not provided.
+- data validation => We can implement data validation in the set() function to make sure that the format or value of the user input is valid.
+
+
+
+
+
+//// 2. Try to explain what is enum and why we use them.
